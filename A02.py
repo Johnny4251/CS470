b26474cdb5514b10cae5bb55183fad188c4b1fe4
John Pertell <pertelj@sunypoly.edu>
2023-10-13 11:42:36 -0400

    Passed all test cases

--- A02.py
+++ A02.py
@@ -12,22 +12,46 @@ def read_kernel_file(filepath):
     row_count = int(tokens[0])
     column_count = int(tokens[1])
 
-    kernel_values = np.zeros((row_count, column_count))
+    kernel = np.zeros((row_count, column_count))
 
     i = 2
     for r in range(row_count):
         for c in range(column_count):
-            kernel_values[r][c] = tokens[i]
+            kernel[r][c] = tokens[i]
             i = i+1
 
     print("Filter: " + file_content)
     print("Kernel Size: " + str(row_count) + "x" + str(column_count))
-    print(kernel_values)
+    print(kernel)
     
-    return kernel_values
+    return kernel
 
-def apply_filter(image, kernel, alpha=1.0, beta=0.0, convert_uint8=True):
-    pass
+def apply_filter(image, kernel, alpha=0.125, beta=127, convert_uint8=True):
+    image = image.astype("float64")
+    kernel = kernel.astype("float64")
+
+    kernel = cv2.flip(kernel, -1)
+    
+    padding_height, padding_width = kernel.shape[0] // 2, kernel.shape[1] // 2
+
+    padded_image = cv2.copyMakeBorder(image, 
+                                      padding_height, padding_height, 
+                                      padding_width, padding_width,
+                                      cv2.BORDER_CONSTANT)
+
+    output = np.zeros((image.shape[0], image.shape[1]), dtype="float64")
+
+    for row in range(image.shape[0]):
+        for col in range(image.shape[1]):
+            subImage = padded_image[ row : (row + kernel.shape[0]), col : (col + kernel.shape[1]) ]
+            filtervals = subImage * kernel
+            value = np.sum(filtervals)
+            output[row,col] = value
+
+    if convert_uint8:
+        output = cv2.convertScaleAbs(output, alpha=alpha, beta=beta)
+
+    return output
 
 def filtering_callback(input_img, filter_file, alpha_val, beta_val):
     input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
@@ -37,19 +61,14 @@ def filtering_callback(input_img, filter_file, alpha_val, beta_val):
 
 def main():
 
-    read_kernel_file("C:\\Users\Johnny\\Documents\\Suny_Poly\\Coursework\\FALL2023\CS470\\CS_470_PERTELJ\\CS_470_PERTELJ\\assign02\\filters\\Filter_001.txt")
-
-    """
     demo = gr.Interface(fn=filtering_callback,
-    inputs=["image",
-    "file",
-    gr.Number(value=0.125),
-    gr.Number(value=127)],
-    outputs=["image"])
+                        inputs=["image",
+                            "file",
+                            gr.Number(value=0.125),
+                            gr.Number(value=127)],
+                        outputs=["image"])
     demo.launch() 
-    """
     
 
-# Later, at the bottom
 if __name__ == "__main__":
     main()
