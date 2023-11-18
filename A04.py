e8eb6664f0be571196c7fca8eb4f608e206f0a78
John Pertell <pertelj@sunypoly.edu>
2023-11-18 09:11:44 -0500

    passed all test cases

--- A04.py
+++ A04.py
@@ -20,9 +20,13 @@ def getOneLBPLabel(subimage, label_type):
         bot_right = subimage[2,2]
 
         # Compute label based off neighbors
-        label = [top_left, top_center, top_right, 
-                 mid_left, mid_right, 
-                 bot_left, bot_center, bot_right]
+        #label = [top_left, top_center, top_right, 
+        #         mid_left, mid_right, 
+        #         bot_left, bot_center, bot_right]
+
+        label = [top_center, top_right, mid_right, 
+                 bot_right, bot_center, bot_left, 
+                 mid_left, top_left]
 
         bin_label = [0] * len(label)
         # Make the lbp label binary using center value as threshold
@@ -33,24 +37,26 @@ def getOneLBPLabel(subimage, label_type):
                 bin_label[i] = 0
 
         m = 0
-        for i in range(len(bin_label)):
-            print("Curr: ",bin_label[i], "Prev: ", bin_label[i-1])
+        for i in range(1, len(bin_label)):
+            #print("Curr: ",bin_label[i], "Prev: ", bin_label[i-1])
             if bin_label[i] != bin_label[i-1]:
-                print("INCREMENTING M")
+                #print("INCREMENTING M")
                 m +=1
 
-        uniform_label = 0
         if m <= 2:
+            #print("SUMMING!")
             uniform_label = sum(bin_label)
         elif m > 2:
+            #print("USING 9")
             uniform_label = 9
 
         #print("SUBIMAGE: ", subimage)
         #return bin_label, m, uniform_label
         return uniform_label
     else:
-        print("other label types are not supported")
+        print("label type is not supported")
         print("leaving function...")
+        return None
 
 def getLBPImage(image, label_type):
     
@@ -64,48 +70,69 @@ def getLBPImage(image, label_type):
     for r in range(image.shape[0]):
         for c in range(image.shape[1]):
             
-            top_left = padded_image[r-1, c-1]
-            top_center = padded_image[r-1, c]
-            top_right = padded_image[r-1, c+1]
+            top_left = padded_image[r, c]
+            top_center = padded_image[r, c+1]
+            top_right = padded_image[r, c+2]
 
-            mid_left = padded_image[r, c-1]
-            center_pixel = padded_image[r,c]
-            mid_right = padded_image[r, c+1]
+            mid_left = padded_image[r+1, c]
+            center = padded_image[r+1,c+1]
+            mid_right = padded_image[r+1, c+2]
 
-            bot_left = padded_image[r+1, c-1]
-            bot_center = padded_image[r+1, c]
-            bot_right = padded_image[r+1, c+1]
+            bot_left = padded_image[r+2, c]
+            bot_center = padded_image[r+2, c+1]
+            bot_right = padded_image[r+2, c+2]
 
             subimage = np.array([[top_left, top_center, top_right],
-                        [mid_left, center_pixel, mid_right],
-                        [bot_left, bot_center, bot_right]])
-            label = getOneLBPLabel(subimage, label_type)
-            #print(label)
-            lbp_image[r,c] = label
+                                 [mid_left, center, mid_right],
+                                 [bot_left, bot_center, bot_right]])
+            lbp_image[r,c] = getOneLBPLabel(subimage, label_type)
     return lbp_image
 
-def main():
-    subimage = np.array([[0, 1, 1],
-                         [0, 0, 1],
-                         [0, 0, 0]])
-    lbp = getOneLBPLabel(subimage, g4.LBP_LABEL_TYPES.UNIFORM)
-    print(lbp)
-#    bin_label, m, uniform_label = getOneLBPLabel(subimage, g4.LBP_LABEL_TYPES.UNIFORM)
-#    print("bin_label: ", bin_label)
- #   print("m: ", m)
- #   print("U_label: ", uniform_label)
+def getOneRegionLBPFeatures(subImage, label_type):
+    if label_type == g4.LBP_LABEL_TYPES.UNIFORM:
+        hist = np.zeros(10, dtype="float32")
 
-    """
-    image = cv2.imread("assign04\images\Image1.png")
-    #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    lbp_image = getLBPImage(image, g4.LBP_LABEL_TYPES.UNIFORM)
+        for row in subImage:
+            for pixel_val in row:
+                hist[pixel_val] += 1
+        
+        total_count = np.sum(hist)
+        normalized_hist = hist / total_count
+        
+        return normalized_hist
+    else:
+        print("label type is not supported")
+        print("leaving function...")
+        return None
 
-    print("READY!")
-    cv2.imshow("window", lbp_image)
-    cv2.waitKey(-1)
-    
-    cv2.destroyAllWindows()
-    """
+def getLBPFeatures(featureImage, regionSideCnt, label_type):
 
-if __name__ == "__main__":
-    main()
+    if label_type == g4.LBP_LABEL_TYPES.UNIFORM:
+
+        height = featureImage.shape[0] // regionSideCnt
+        width = featureImage.shape[1] // regionSideCnt
+
+        image_hist = []
+
+        for i in range(regionSideCnt):
+            for j in range(regionSideCnt):
+                start_row = i * height
+                end_row = start_row + height
+                start_col = j * width
+                end_col = start_col + width
+                sub_image = featureImage[start_row:end_row, start_col:end_col]
+                
+                
+                hist = getOneRegionLBPFeatures(sub_image, g4.LBP_LABEL_TYPES.UNIFORM)
+
+                image_hist.append(hist)
+
+        image_hist = np.array(image_hist)
+        image_hist = np.reshape(image_hist, (image_hist.shape[0]*image_hist.shape[1],))
+
+    else:
+        print("label type is not supported")
+        print("leaving function...")
+        return None
+
+    return image_hist
